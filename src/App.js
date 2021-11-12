b7274b2140d28cdc46793c6ad08f5474cad2549f
mac_03 <ma.sargsyan@it-justice.com>
2021-11-12 16:34:01 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React,{useState} from 'react'
 import {Switch,Route} from 'react-router-dom';
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
--- src/mock/events.js
+++
@@ -1,9 +0,0 @@
-export const events =  [
-	{
-		id: 0,
-		title: "All Day Event very long title",
-		allDay: true,
-		start: new Date(2021, 9, 0),
-		end: new Date(2021, 9, 0)
-	},
-]
